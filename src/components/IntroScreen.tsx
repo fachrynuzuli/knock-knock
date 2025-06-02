1beab56fda1b26f273a0d830291169d543aa230c
fachrynuzuli <fachrynuzuli@gmail.com>
2025-06-03 03:11:42 +0700

    Updated IntroScreen.tsx

--- src/components/IntroScreen.tsx
+++ src/components/IntroScreen.tsx
@@ -114,7 +114,7 @@ const IntroScreen: React.FC<IntroScreenProps> = ({ onStartGame }) => {
                       width: '32px',
                       height: '48px',
                       backgroundImage: `url("${getAvatarSprite(avatarOptions[currentAvatarIndex])}")`,
-                      backgroundPosition: '-15px -10px',
+                      backgroundPosition: '-12px -10px',
                       transform: 'scale(1.5)',
                       transformOrigin: 'center',
                     }}
