59ebdc1662100ec1e3092c837fc56db5ffdbec87
fachrynuzuli <fachrynuzuli@gmail.com>
2025-06-03 02:20:47 +0700

    Updated IntroScreen.tsx

--- src/components/IntroScreen.tsx
+++ src/components/IntroScreen.tsx
@@ -29,7 +29,7 @@ const IntroScreen: React.FC<IntroScreenProps> = ({ onStartGame }) => {
           Knock Knock,Shippers!
         </h1>
         <p className="text-xl md:text-2xl font-pixel text-white">
-          Non-boring reporting sheet
+          A non-boring task reporting sheet
         </p>
       </div>
 
