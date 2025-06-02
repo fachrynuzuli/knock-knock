22c81ca3c0650439b596f572647f278e646adb27
fachrynuzuli <fachrynuzuli@gmail.com>
2025-06-03 01:33:02 +0700

    Updated Game.tsx

--- src/components/Game.tsx
+++ src/components/Game.tsx
@@ -213,7 +213,7 @@ const Game: React.FC = () => {
         className="absolute"
         style={{
           left: '400px',
-          top: '400px',
+          top: '200px',
         }}
       >
         <div className="house" style={{ width: '64px', height: '64px' }}>
