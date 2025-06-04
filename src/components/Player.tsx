9b873d350ae662b3bf0a5f5f8fde457d98e159b3
fachrynuzuli <fachrynuzuli@gmail.com>
2025-06-04 12:51:51 +0700

    Updated Player.tsx

--- src/components/Player.tsx
+++ src/components/Player.tsx
@@ -15,7 +15,7 @@ interface PlayerProps {
 const spriteConfig = {
   1: {
     path: '/Unarmed_Walk_full.png',
-    frameWidth: 64, 
+    frameWidth: 64,
     frameHeight: 64,
     frameCount: 6,
     idleFrame: 0,
