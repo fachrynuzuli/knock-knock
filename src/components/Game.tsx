ae217eba42c9612fb7846391b5c7f308fcb05344
Fachry Nuzuli Kamal <fachrynuzuli@gmail.com>
2025-06-07 10:09:39 +0700

    Updated Game.tsx

--- src/components/Game.tsx
+++ src/components/Game.tsx
@@ -315,7 +315,7 @@ const Game: React.FC = () => {
             top: `${townHallPosition.y}px`,
           }}
         >
-          <div className="w-150 h-80 bg-primary-600 bg-opacity-50 border-4 border-primary-800 rounded-lg flex items-center justify-center">
+          <div className="w-140 h-80 bg-primary-600 bg-opacity-50 border-4 border-primary-800 rounded-lg flex items-center justify-center">
             <div className="text-white text-sm font-pixel text-center">Town Hall</div>
           </div>
         </div>
