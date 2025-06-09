318bf73f56a3038fb8364cde5529eede32244259
Fachry Nuzuli Kamal <fachrynuzuli@gmail.com>
2025-06-09 14:52:22 +0700

    Updated IntroScreen.tsx

--- src/components/IntroScreen.tsx
+++ src/components/IntroScreen.tsx
@@ -267,13 +267,13 @@ const IntroScreen: React.FC<IntroScreenProps> = ({ onStartGame }) => {
         setTimeout(() => {
           setLockedMessage(validation.message || 'This avatar is locked.');
           setTimeout(() => setLockedMessage(''), 3000);
-        }, 100);
+        }, 1);
       } else {
         // If unlocked, select it after centering
         setTimeout(() => {
           setSelectedAvatarId(clickedAvatarId);
           console.log('AVATAR SELECTION: Selected avatar after centering:', clickedAvatarId, avatar?.name);
-        }, 100);
+        }, 1);
       }
     }
   };
